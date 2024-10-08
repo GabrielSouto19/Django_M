326663c481eff5878740fe9b45c462a14ee1ae85
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2024-10-08 10:43:56 -0300

    CBV's reestruturando minhas views agora baseadas em classes

--- Carros/cars/views.py
+++ Carros/cars/views.py
@@ -1,31 +1,37 @@
 from django.shortcuts import render,redirect
 from .models import Car
 from .forms import CarModelForm
+from django.views import View
 
 # Create your views here.
-def show_cars(request):
-    cars = Car.objects.all().order_by("model")
-    search = request.GET.get("search")
 
-    if search:# somente verifica se ela não esta vazia
-        cars = Car.objects.filter(model__icontains=search)# o underline duplo possibilita a navegação entre tabelas do django
-        print(cars)
+class CarsView(View):
+    def get(self,request):
+        cars = Car.objects.all().order_by("model")
+        search = request.GET.get("search")
 
-
-    context = {
-        "cars":cars
-    }
-    return render(request,'cars.html',context=context)
+        if search:# somente verifica se ela não esta vazia
+            cars = Car.objects.filter(model__icontains=search)# o underline duplo possibilita a navegação entre tabelas do django
+            print(cars)
 
 
-  
-def new_car(request):
-    if request.method == "POST":
+        context = {
+            "cars":cars
+        }
+        return render(request,'cars.html',context=context)
+
+
+
+class NewCarView(View):
+    def get(self,request):
+        new_car_form = CarModelForm()   
+        return render(request,"new_car.html",{"new_car_form":new_car_form})
+
+
+    def post(self,request):
         new_car_form = CarModelForm(request.POST,request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()    
             return redirect('cars_list')
-            
-    else:
-        new_car_form = CarModelForm()
-    return render(request,"new_car.html",{"new_car_form":new_car_form})
+        return render(request,"new_car.html",{"new_car_form":new_car_form})
+
