6437a6e2b1c852290dcfc5911878e5110bb8cfdc
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2024-10-08 11:19:44 -0300

    Melhorando as views usando herança de classes pra facilitar nosso lado em ListView

--- Carros/app/urls.py
+++ Carros/app/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsView,NewCarView
+from cars.views import NewCarView,CarsListView
 from accounts.views import register_user,login_view,logout_view
 
 
@@ -14,6 +14,6 @@ urlpatterns = [
     path('register/',view=register_user,name="register_user"),
     path('login/',view=login_view,name="login"),
     path('logout/',view=logout_view,name="logout"),
-    path('cars/',view=CarsView.as_view(),name="cars_list"),
+    path('cars/',view=CarsListView.as_view(),name="cars_list"),
     path('new_car/',view=NewCarView.as_view(),name="new_car"),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- Carros/cars/views.py
+++ Carros/cars/views.py
@@ -2,25 +2,21 @@ from django.shortcuts import render,redirect
 from .models import Car
 from .forms import CarModelForm
 from django.views import View
-
-# Create your views here.
-
-class CarsView(View):
-    def get(self,request):
-        cars = Car.objects.all().order_by("model")
-        search = request.GET.get("search")
-
-        if search:# somente verifica se ela não esta vazia
-            cars = Car.objects.filter(model__icontains=search)# o underline duplo possibilita a navegação entre tabelas do django
-            print(cars)
+from django.views.generic import ListView
 
 
-        context = {
-            "cars":cars
-        }
-        return render(request,'cars.html',context=context)
 
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get("search")
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
 
 class NewCarView(View):
     def get(self,request):
